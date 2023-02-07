0118eac65f552bd5e59a51f1a89ec78b4eb1e587
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-02-07 22:51:47 +0500

    initialized server.go & added few dependencies

---
+++ server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	pb "grpc/proto"
+	"net"
+)
+
+const (
+	port = ":8000"
+)
+
+type welcome struct {
+	pb.WelcomeServiceServer
+}
+
+func main() {
+	listen,err := net.Listen("tcp", port)
+	checkFatalError(err)
+
+	grpcServer := grpc.NewServer()
+	// Register the service
+	pb.RegisterWelcomeServiceServer(grpcServer, &welcome{})
+	err = grpcServer.Serve(listen)
+	checkFatalError(err)
+}
+
+func checkFatalError(err error)  {
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+}
